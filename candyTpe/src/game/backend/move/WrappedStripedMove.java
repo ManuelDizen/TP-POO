0c9a7c7fdaaa6ea1a51f0e6fba4cff65edc807c2
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-05 15:54:25 -0300

    Update WrappedStripedMove.java
    Se eliminaron múltiples for

--- candyTpe/src/game/backend/move/WrappedStripedMove.java
+++ candyTpe/src/game/backend/move/WrappedStripedMove.java
@@ -15,15 +15,12 @@ public class WrappedStripedMove extends Move {
 				if (i2 + i >= 0 && i2 + i < Grid.SIZE) {
 					clearContent(i2 + i, j);
 				}
-			}
-		}
-		for(int j = -1; j < 2; j++) {
-			for(int i = 0; i < Grid.SIZE; i++) {
-				if (j2 + j >= 0 && j2 + j < Grid.SIZE) {
-					clearContent(i, j2 + j);
+				if (j2 + i >= 0 && j2 + i < Grid.SIZE) {
+					clearContent(j, j2 + i);
 				}
 			}
 		}
+
 	}
 
 }

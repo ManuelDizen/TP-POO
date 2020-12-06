5bac51929b155b2bd2668dc7fda595debd087962
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-06 10:03:20 -0300

    Se elimina fillCells y se pasa a la clase Grid

--- candyTpe/src/game/backend/level/Level2.java
+++ candyTpe/src/game/backend/level/Level2.java
@@ -13,42 +13,11 @@ public class Level2 extends Grid {
     private int filledCells = 0;
     protected boolean isGolden[][] = new boolean[SIZE][SIZE];
 
-    private Cell wallCell;
-    private Cell candyGenCell;
-
     @Override
     protected GameState newState() {
         return new Level2.Level2State(MAX_MOVES, MAX_CELLS);
     }
 
-    @Override
-    protected void fillCells() {
-
-        wallCell = new Cell(this);
-        wallCell.setContent(new Wall());
-        candyGenCell = new CandyGeneratorCell(this);
-
-        //corners
-        g()[0][0].setAround(candyGenCell, g()[1][0], wallCell, g()[0][1]);
-        g()[0][SIZE-1].setAround(candyGenCell, g()[1][SIZE-1], g()[0][SIZE-2], wallCell);
-        g()[SIZE-1][0].setAround(g()[SIZE-2][0], wallCell, wallCell, g()[SIZE-1][1]);
-        g()[SIZE-1][SIZE-1].setAround(g()[SIZE-2][SIZE-1], wallCell, g()[SIZE-1][SIZE-2], wallCell);
-
-        for (int j = 1; j < SIZE-1; j++) {
-            //upper line cells
-            g()[0][j].setAround(candyGenCell,g()[1][j],g()[0][j-1],g()[0][j+1]);
-            //bottom line cells
-            g()[SIZE-1][j].setAround(g()[SIZE-2][j], wallCell, g()[SIZE-1][j-1],g()[SIZE-1][j+1]);
-            //left line cells
-            g()[j][0].setAround(g()[j-1][0],g()[j+1][0], wallCell ,g()[j][1]);
-            //right line cells
-            g()[j][SIZE-1].setAround(g()[j-1][SIZE-1],g()[j+1][SIZE-1], g()[j][SIZE-2], wallCell);
-            //central cells
-            for (int i = 1; i < SIZE-1; i++) {
-                g()[j][i].setAround(g()[j-1][i],g()[j+1][i],g()[j][i-1],g()[j][i+1]);
-            }
-        }
-    }
     private int getFilledCells(){
         return filledCells;
     }

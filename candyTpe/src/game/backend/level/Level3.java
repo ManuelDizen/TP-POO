12c6ef6dff613a34a8adc9b40dc7cb651070f404
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-06 10:04:12 -0300

    Se agregó el nivel 3

---
+++ candyTpe/src/game/backend/level/Level3.java
@@ -0,0 +1,58 @@
+package game.backend.level;
+
+import game.backend.GameState;
+import game.backend.Grid;
+import game.backend.cell.CandyGeneratorCell;
+import game.backend.cell.Cell;
+import game.backend.element.Wall;
+
+public class Level3 extends Grid {
+
+    private static int MAX_MOVES = 20;
+    private static int CENTER = 4;
+    private int jailsLeft = 8;
+    private boolean hasJail[] = new boolean[SIZE];
+
+    public Level3(){
+        for (int i = 0; i < SIZE; i++)
+            if (i != CENTER)
+                hasJail[i] = true;
+    }
+
+    @Override
+    protected GameState newState() {
+        return new Level3.Level3State(MAX_MOVES);
+    }
+
+    private int getJailsLeft(){
+        return jailsLeft;
+    }
+
+    @Override
+    public boolean tryMove(int i1, int j1, int i2, int j2) {
+        boolean ret;
+        if ((i1 == CENTER && hasJail[j1]) || (i2 == CENTER && hasJail[j2]))
+            return false;
+        if (ret = super.tryMove(i1, j1, i2, j2)) {
+            state().addMove();
+        }
+        return ret;
+    }
+
+    private class Level3State extends GameState {
+        private long maxMoves;
+
+        public Level3State(int maxMoves) {
+            this.maxMoves = maxMoves;
+            super.setMoves(maxMoves);
+        }
+
+        public boolean gameOver() {
+            return playerWon() || getMoves() >= maxMoves;
+        }
+
+        public boolean playerWon() {
+            return getJailsLeft() == 0;
+        }
+    }
+}

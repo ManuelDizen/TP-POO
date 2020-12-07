77db8f3c809e714ddcd580b26665a71f33f88827
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-07 17:26:31 -0300

    Agregamos comentarios

--- candyTpe/src/game/frontend/GameApp.java
+++ candyTpe/src/game/frontend/GameApp.java
@@ -21,27 +21,33 @@ public class GameApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.stage = primaryStage;
-
+		//Creamos un choiceBox para que el usuario decida a qué nivel jugar
 		ChoiceBox<String> levels = new ChoiceBox<>();
-		levels.getItems().addAll("Nivel 1","Nivel 2", "Nivel 3");
+		//Añadimos los niveles
+		levels.getItems().addAll("Básico","Golden Board", "Wall Blast");
+		//Creamos el boton jugar
 		Button button = new Button("Jugar");
+		//Cuando se apriete el botón, se obtiene el valor del choiceBox
 		button.setOnAction(e -> levelSelection(levels.getValue()));
-
+		//Ponemos un título antes del choiceBox
 		Label title = new Label("Seleccione un nivel:   ");
+		//Ponemos el titulo, el choiceBox y el botón en formato horizontal
 		HBox menu = new HBox(title, levels, button);
-
-		StackPane stackScreen = new StackPane(menu);
-		stage.setScene(new Scene(stackScreen));
+		//Usamos un BorderPane para mostrar el menú
+		Pane display = new BorderPane(menu);
+		//Seteamos el escenario con los elementos de antes
+		stage.setScene(new Scene(display));
 
 		stage.setResizable(false);
 		stage.show();
 	}
 	
+	//Recibe la opción del choiceBox e inicia el nivel correspondiente
 	private void levelSelection(String level){
 		Class selectedLevel;
-		if (level.equals("Nivel 1"))
+		if (level.equals("Básico"))
 			selectedLevel = Level1.class;
-		else if (level.equals("Nivel 2"))
+		else if (level.equals("Golden Board"))
 			selectedLevel = Level2.class;
 		else
 			selectedLevel = Level3.class;

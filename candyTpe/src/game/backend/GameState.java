d912facf8c041891ad149dec4956203f566a90b2
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-07 17:06:39 -0300

    Agregamos comentarios y algunos métodos

--- candyTpe/src/game/backend/GameState.java
+++ candyTpe/src/game/backend/GameState.java
@@ -4,7 +4,10 @@ public abstract class GameState {
 	
 	private long score = 0;
 	private int moves = 0;
+	//Cantiad de movimientos por default (de todas formas, definimos el método setMaxMoves para que se pueda modificar en los niveles)
 	private static final int MAX_MOVES = 20;
+	//Cantiad de celdas especiales por default (de todas formas, definimos el método setSpecialCells para que se pueda modificar en los niveles)
+	private int specialCells = 0;
 	
 	public void addScore(long value) {
 		this.score = this.score + value;
@@ -22,11 +25,25 @@ public abstract class GameState {
 		return moves;
 	}
 	
+	//devuelve la cantidad de movimientos que faltan para perder
 	public Integer getMovesLeft() {
 		return MAX_MOVES - moves;
 	}
 	
-	public abstract Integer getSpecialCells();
+	//devuelve la cantidad de special cells en el tablero
+	public Integer getSpecialCells() {
+		return specialCells;
+	}
+	
+	//decrementa las special cells
+	public void deleteSpecialCells(){
+		specialCells--;
+	}
+
+	//Seteamos la cantidad inicial de special cells
+	public void setSpecialCells(int specialCells){
+		this.specialCells = specialCells;
+	}
 
 	public boolean gameOver(){
 		return playerWon() || getMoves() >= MAX_MOVES;
@@ -34,4 +51,7 @@ public abstract class GameState {
 	
 	public abstract boolean playerWon();
 
+	public void setMaxMoves(int moves){
+		this.MAX_MOVES = moves;
+	}
 }

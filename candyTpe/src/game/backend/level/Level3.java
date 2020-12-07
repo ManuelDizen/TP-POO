511cb9a1ef04ebc1b593a0dab76fcb653e4d688b
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-07 17:03:22 -0300

    Modificaciones a wallBlast

--- candyTpe/src/game/backend/level/Level3.java
+++ candyTpe/src/game/backend/level/Level3.java
@@ -2,17 +2,19 @@ package game.backend.level;
 
 import game.backend.GameState;
 import game.backend.Grid;
-import game.backend.cell.Cell;
-import game.backend.element.*;
 
 public class Level3 extends Grid {
+    //Cantidad de celdas wallBlast
     private static final int MAX_CELLS = 9;
+    //Centro del tablero
     private static final int CENTER = 3;
-    private int cellsLeft = MAX_CELLS;
+    //Cantidad máxima de movimientos
+    private static final int MAX_MOVES = 30;
 
     @Override
     protected void fillCells() {
         super.fillCells();
+        //Seteamos las 9 celdas del medio como wallBlast
         for (int i = CENTER; i < 2*CENTER; i++)
             for (int j = CENTER; j < 2*CENTER; j++)
                 g()[i][j].setWallBlast();
@@ -23,111 +25,26 @@ public class Level3 extends Grid {
         return new Level3.Level3State();
     }
 
-    private int getCellsLeft(){
-        return cellsLeft;
-    }
-
     @Override
     public boolean tryMove(int i1, int j1, int i2, int j2) {
-        boolean ret;
-        Element e1 = g()[i1][j1].getContent();
-        Element e2 = g()[i2][j2].getContent();
-        Cell[][] grid = g();
         if (ret = super.tryMove(i1, j1, i2, j2)) {
             state().addMove();
-            checkForBomb(e1, e2, grid);
-            if (e1.equals(g()[i2][j2]))
-                checkForSpecialCandy(e2, i1, j1);
-            else
-                checkForSpecialCandy(e1, i2, j2);
         }
         return ret;
     }
 
-    private void checkForBomb(Element e1, Element e2, Cell[][] grid){
-        if (e1 instanceof Bomb && e2 instanceof Bomb){
-            deleteAll();
-        }
-        else if (e1 instanceof Bomb && e2 instanceof Candy) {
-            Candy e = (Candy) e2;
-            checkGridForWallBlast(e.getColor(), grid);
-        }
-        else if (e2 instanceof Bomb && e1 instanceof Candy) {
-            Candy e = (Candy) e1;
-            checkGridForWallBlast(e.getColor(), grid);
-        }
-    }
-
-    private void deleteAll(){
-        for (int i = CENTER; i < 2 * CENTER; i++){
-            for (int j = CENTER; j < 2 * CENTER; j++) {
-                g()[i][j].setWallBlastFalse();
-                cellsLeft--;
-            }
-        }
-    }
-
-    private void checkForSpecialCandy(Element e, int row, int col){
-        if (e instanceof HorizontalStripedCandy)
-            checkRowForWallBlast(row);
-        else if (e instanceof VerticalStripedCandy)
-            checkColumnForWallBlast(col);
-        else if (e instanceof WrappedCandy)
-            checkSquareForWallBlast(row, col);
-    }
-
-    private void checkRowForWallBlast(int i){
-        for (int j = 0; j < SIZE; j++){
-            if (g()[i][j].isWallBlast()){
-                g()[i][j].setWallBlastFalse();
-                cellsLeft--;
-            }
-        }
-    }
-
-    private void checkColumnForWallBlast(int j){
-        for (int i = 0; i < SIZE; i++){
-            if (g()[i][j].isWallBlast()){
-                g()[i][j].setWallBlastFalse();
-                cellsLeft--;
-            }
-        }
-    }
-
-    private void checkSquareForWallBlast(int i, int j){
-        if (i > 0 && i < SIZE && j > 0 && j < SIZE) {
-            for (int row = i - 1; row < i + i; i++)
-                for (int col = j - 1; col < j + 1; j++)
-                    if (g()[row][col].isWallBlast()) {
-                        g()[row][col].setWallBlastFalse();
-                        cellsLeft--;
-                    }
-        }
-    }
-
-    private void checkGridForWallBlast(CandyColor color, Cell[][] grid){
-        for (int i = CENTER; i < 2 * CENTER; i++){
-            for (int j = CENTER; j < 2 * CENTER; j++) {
-                if (grid[i][j].isWallBlast() && grid[i][j].getContent() instanceof Candy) {
-                    Candy aux = (Candy) grid[i][j].getContent();
-                    if (aux.getColor().equals(color)){
-                        g()[i][j].setWallBlastFalse();
-                        cellsLeft--;
-                    }
-                }
-            }
-        }
-    }
-
     private class Level3State extends GameState {
-
-        public boolean playerWon() {
-            return getCellsLeft() == 0;
+        
+        //Seteamos la cantidad de special cells
+        public Level3State(){
+            this.setSpecialCells(MAX_CELLS);
+            //Cambiamos la cantidad máxima de movimientos del default
+            this.setMaxMoves(MAX_MOVES);
         }
 
-        @Override
-        public Integer getSpecialCells() {
-            return cellsLeft;
+        //El jugador gana cuando no hay mas specialCells
+        public boolean playerWon() {
+            return this.getSpecialCells() == 0;
         }
     }
 }

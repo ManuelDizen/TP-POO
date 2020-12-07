ef91c24ad9d6640cda49d6b6589ba42736d3afa5
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-07 17:19:30 -0300

    TwoWrappedMove extiende de SpecialMove y modularizamos

--- candyTpe/src/game/backend/move/TwoWrappedMove.java
+++ candyTpe/src/game/backend/move/TwoWrappedMove.java
@@ -2,7 +2,9 @@ package game.backend.move;
 
 import game.backend.Grid;
 
-public class TwoWrappedMove extends Move {
+//TwoWrappedMove extiende de SpecialMove
+public class TwoWrappedMove extends SpecialMove {
+	private int currI, currJ;
 	
 	public TwoWrappedMove(Grid grid) {
 		super(grid);
@@ -10,35 +12,33 @@ public class TwoWrappedMove extends Move {
 	
 	@Override
 	public void removeElements() {
-		int currI, currJ;
 		if (i1 == i2) {
-			if (j1 < j2) {
-				currI = i1;
-				currJ = j1;
-			} else {
-				currI = i2;
-				currJ = j2;
-			}
-			clearContent(currI,currJ-1);
-			clearContent(currI, currJ + 2);
-			for(int n = -1; n < 3; n++) {
-				clearContent(currI - 1, currJ + n);
-				clearContent(currI + 1, currJ + n);
-			}
+			if (j1 < j2)
+				setCurrent(i1, j1);
+			else
+				setCurrent(i2, j2);
 		} else {
-			if (i1 < i2) {
-				currI = i1;
-				currJ = j1;
-			} else {
-				currI = i2;
-				currJ = j2;
-			}
-			clearContent(currI,currJ-1);
-			clearContent(currI,currJ+2);
-			for(int n = -1; n < 3; n++) {
-				clearContent(currI - 1, currJ + n);
-				clearContent(currI + 1, currJ + n);
-			}
+			if (i1 < i2)
+				setCurrent(i1, j1);
+			else
+				setCurrent(i2, j2);
+		}
+		clear(currI, currJ);
+	}
+	
+	//Creamos los métodos setCurrent y clear para modularizar y no repetir código
+
+	private void setCurrent(int i, int j){
+		currI = i;
+		currJ = j;
+	}
+
+	private void clear(int i, int j){
+		clearContent(i,j-1);
+		clearContent(i, j + 2);
+		for(int n = -1; n < 3; n++) {
+			clearContent(i - 1, j + n);
+			clearContent(i + 1, j + n);
 		}
 	}
 

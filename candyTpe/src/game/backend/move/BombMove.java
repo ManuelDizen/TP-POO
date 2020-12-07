18cd7266434306ffee6dfd5deb8ef3e092f2814d
mcornidez <67882015+mcornidez@users.noreply.github.com>
2020-12-07 17:12:04 -0300

    Modularizamos

--- candyTpe/src/game/backend/move/BombMove.java
+++ candyTpe/src/game/backend/move/BombMove.java
@@ -4,6 +4,7 @@ import game.backend.Grid;
 import game.backend.element.Bomb;
 import game.backend.element.Candy;
 
+//BombMove extiende de SpecialMove
 public class BombMove extends Move {
 	
 	public BombMove(Grid grid) {
@@ -15,6 +16,11 @@ public class BombMove extends Move {
 		Candy candy = (Candy) (get(i1, j1) instanceof Bomb ? get(i2, j2) : get(i1, j1));
 		clearContent(i1, j1);
 		clearContent(i2, j2);
+		clearCells(candy);
+	}
+	
+	//Creamos el método clearCells para que la puedan utilizar las clases hijas (por eso es protected)
+	protected void clearCells(Candy candy){
 		for(int i = 0; i < Grid.SIZE; i++) {
 			for(int j = 0; j < Grid.SIZE; j++) {
 				if (candy.equals(get(i, j))) {
